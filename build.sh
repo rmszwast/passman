4aec0920f3b08d2a3ac7dd2d0b0a03c86094f780
Ryan <>
2024-10-23 02:43:32 +0000

    Modified build script arg=release builds to passman/release with no debug symbols. Builds to passman/debug with debug symbols otherwise.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cmake -DCMAKE_BUILD_TYPE=Debug -S ${PROJECT_SOURCE_DIR} -B ${PROJECT_SOURCE_DIR}/debug
-cmake --build ${PROJECT_SOURCE_DIR}/debug
+if [[ $1 == release ]]; then
 cmake -DCMAKE_BUILD_TYPE=Release -S ${PROJECT_SOURCE_DIR} -B ${PROJECT_SOURCE_DIR}/release
 cmake --build ${PROJECT_SOURCE_DIR}/release
+else
+cmake -DCMAKE_BUILD_TYPE=Debug -S ${PROJECT_SOURCE_DIR} -B ${PROJECT_SOURCE_DIR}/debug
+cmake --build ${PROJECT_SOURCE_DIR}/debug
+fi;
