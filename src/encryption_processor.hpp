23752a80811f9c013a68c3e0ad4135864e21146c
Elizabeth <ogleznee@oregonstate.edu>
2024-11-02 02:15:06 +0000

    Added comments, removed old encryption example

--- src/encryption_processor.hpp
+++ src/encryption_processor.hpp
@@ -19,9 +19,9 @@ class Encryptor {
 public:
   Encryptor();
   std::vector<uint8_t> encrypt(std::string_view password,
-                               std::string_view stringToEncrypt = "");
+                               std::string_view stringToEncrypt = ""); // Takes string password and string data for encryption (defaults to null string)
   Botan::secure_vector<uint8_t> decrypt(std::string_view password,
-                                        std::span<const uint8_t> encryptedData);
+                                        std::span<const uint8_t> encryptedData); // Takes string password and encrypted data
 
 private:
   static constexpr size_t SALT_STR_LENGTH = 16;
