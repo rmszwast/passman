5402fb55661e6e100ff36cc45966463167603ee1
isaac-johnson <johnsonisaacn@gmail.com>
2024-11-08 18:07:33 -0800

    added error handling and sanitization for password clip

--- src/cli/clip.cpp
+++ src/cli/clip.cpp
@@ -23,41 +23,34 @@ Clip::setup()
     app = app->add_subcommand(name, description);
     app->usage("passman clip COMMAND [OPTIONS]");
     app->add_option("clip", "clip = copy to clipboard");
-    /**
-     * Should we inherit the following from Command?
-     * timout
-     * attributeOption
-     * TotpOption
-     * bestMatchOption
-     * currentVault
-     * positionalArguments
-     * optionalArguments
-     * options
-     *
-     */
+
 }
 
-/* TODO: Implement argument parsing, timeout functionality,
-option selection, exit handling, display to user */
 
 int
 Clip::execute(Entry entry, int timeout = DEFAULT_CLIPBOARD_TIMEOUT)
 {
-  /**
-   * Need to modify to escape special characters that
-   * may be in password. Also need to consider using 
-   * exec family functions instead of system
-   */
   if (timeout <= 0) {
     return 0;
   }
   std::string password = entry.getPassword;
   //sanitize password here
+  std::string chars_to_escape = "\\\"$`<>|;&";
+  for (char c : chars_to_escape) {
+    size_t pos = 0;
+        while ((pos = password.find(c, pos)) != std::string::npos) {
+            password.insert(pos, "\\");
+            pos += 2;
+        }
+
+  }
+  //copy to clipboard
   std::string cmd = "echo " + password + " | xclip -selection clipboard";
   int res = system(cmd.c_str());
   if (res != 0) {
     std::cout << "Command failed with code: " << res << std::endl;
   }
+  //clear clipboard after set amount of time
   while (timeout > 0) {
     std::cout << "clearing clipboard in " << timeout << " seconds";
     sleep(1);
@@ -69,7 +62,6 @@ Clip::execute(Entry entry, int timeout = DEFAULT_CLIPBOARD_TIMEOUT)
   std::cout << "Clipboard cleared" << std::endl;
   system("xclip /dev/null");
   system("xclip -selection clipboard /dev/null");
-
 
   return 0;
 }
