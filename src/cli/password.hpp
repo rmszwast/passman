43d61081f666a817e69b1219d86cb2a441889c6f
James Kopatsis <jkopatsis@gmail.com>
2024-11-21 22:18:00 -0800

    removed unused function for password.hpp

--- src/cli/password.hpp
+++ src/cli/password.hpp
@@ -8,22 +8,6 @@ class Password : public Command
   public:
     Password(CLI::App* app);
     void setup();
-
-  private:
-    void generatePassword(int minLength,
-                          int maxLength,
-                          bool upperRequired,
-                          bool lowerRequired,
-                          bool numRequired,
-                          bool specialRequired,
-                          bool upperNotAllowed,
-                          bool lowerNotAllowed,
-                          bool numNotAllowed,
-                          bool specialNotAllowed,
-                          bool noDictionaryWords,
-                          bool noRepeatCharacters);
-
-    void generateDicewarePassword(int dicewareWordCount);
 };
 
 #endif
